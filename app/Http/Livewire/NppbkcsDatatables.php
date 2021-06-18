c3710157f565f39a4330af72f577a08a52e18e55
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-06-19 02:41:32 +0700

    add data table

---
+++ app/Http/Livewire/NppbkcsDatatables.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class NppbkcsDatatables extends Component
+{
+    public function render()
+    {
+        return view('livewire.nppbkcs-datatables');
+    }
+}
