727ffcf9ef26c7140b362860523358897feb7b14
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-06-19 14:46:23 +0700

    fix nppbkc table

--- database/factories/NppbkcFactory.php
+++ database/factories/NppbkcFactory.php
@@ -23,11 +23,61 @@ class NppbkcFactory extends Factory
     {
         return [
             'nama_pemilik' => $this->faker->name,
-            'status_nppbkc' =>  $this->faker->randomElement(['Pengajuan', 'Pemeriksaan Lokasi', 'Ditolak','Disetujui']),
+            //'status_nppbkc' =>  $this->faker->randomElement(['Pengajuan', 'Pemeriksaan Lokasi', 'Ditolak','Disetujui']),
+            'status_nppbkc' =>  $this->faker->randomElement([1,2,3,4]),
             'status_pemohon' =>  $this->faker->randomElement(['sendiri', 'dikuasakan']),
             'email_pemilik' => $this->faker->unique()->safeEmail,
+            'alamat_pemilik' => $this->faker->address,
+            
+            'jenis_usaha_bkc'=>$this->faker->randomElement([
+                'Pengusaha Pabrik',
+                'Pengusaha Tempat Penyimpanan',
+                'Importir',
+                'Penyalur',
+                'Pengusaha Tempat Penjualan Eceran'
+            ]),
+            
+            'jenis_bkc'=>$this->faker->randomElement([
+                'Hasil Tembakau',
+                'Hasil Pengolahan Tembakau Lainnya',
+                'Minuman Mengandung Etil Alkohol',
+                'Etil Alkohol'
+            ]),
+
+            'nama_perusahaan' => $this->faker->name,
+            'alamat_perusahaan' => $this->faker->address,
             'email_perusahaan' => $this->faker->unique()->safeEmail,
-            'lokasi' => $this->faker->address
+            'jenis_lokasi'=>$this->faker->randomElement([
+                'Pabrik',
+                'Tempat Penyimpanan',
+                'Tempat Usaha Importir',
+                'Tempat Penjualan Eceran'
+            ]),
+
+            'lokasi' => $this->faker->address,
+            'kegunaan'=>$this->faker->randomElement([
+                'Membuat Barang Kena Cukai',
+                'Mengemas Barang Kena Cukai',
+                'Menyimpan Bahan Baku atau Bahan Penolong',
+                'Menimbun Barang Kena Cukai yang Selesai Dibuat',
+                'Menimbun Barang Kena Cukai yang Sudah Dilunasi Cukainya'
+            ]),
+
+            'provinsi' => $this->faker->state,
+            'kota' => $this->faker->city,
+            'kecamatan' => $this->faker->city,
+            'kelurahan' => $this->faker->city,
+            'alamat' => $this->faker->address,
+            'lokasi_latitude' => $this->faker->latitude,
+            'lokasi_longitude' => $this->faker->latitude,
+
+            'jenis_izin'=>$this->faker->randomElement([
+                'Izin Usaha Industri',
+                'Surat Izin Usaha Perdagangan'
+            ]),
+            'tanggal_izin_from'=>$this->faker->date,
+            'tanggal_izin_to'=>$this->faker->date,
+            'tanggal_kesiapan_cek_lokasi'=>$this->faker->date
         ];
     }
 }
--- database/migrations/2021_06_18_112818_create_nppbkc_table.php
+++ database/migrations/2021_06_18_112818_create_nppbkc_table.php
@@ -15,39 +15,56 @@ class CreateNppbkcTable extends Migration
     {
         Schema::create('nppbkcs', function (Blueprint $table) {
             $table->id();
-            $table->string('status_nppbkc');
+            $table->tinyInteger('status_nppbkc')->default('1');
+            
             $table->string('status_pemohon');
             $table->string('nama_pemilik');
-
-            //remove theses
-            $table->string('lokasi');
+            $table->string('alamat_pemilik');
+            $table->string('npwp_pemilik')->nullable();
+            $table->string('telp_pemilik')->nullable();
             $table->string('email_pemilik');
+
+            $table->string('jenis_usaha_bkc');
+            $table->string('jenis_bkc');
+
+            $table->string('nama_perusahaan');
+            $table->string('alamat_perusahaan');
+            $table->string('npwp_perusahaan')->nullable();
+            $table->string('telp_perusahaan')->nullable();
             $table->string('email_perusahaan');
-            //end remove
+            $table->string('jenis_lokasi');
 
-            // $table->string('alamat_pemilik');
-            // $table->string('npwp_pemilik');
-            // $table->string('telp_pemilik');
-            // $table->string('email_pemilik');
+            $table->string('lokasi');
+            $table->string('kegunaan');
+            $table->string('provinsi');
+            $table->string('kota');
+            $table->string('kecamatan');
+            $table->string('kelurahan');
+            $table->string('rt_rw')->nullable();
+            $table->string('alamat');
+            $table->double('lokasi_latitude')->nullable();
+            $table->double('lokasi_longitude')->nullable();
 
-            // $table->string('nama_perusahaan');
-            // $table->string('alamat_perusahaan');
-            // $table->string('npwp_perusahaan');
-            // $table->string('telp_perusahaan');
-            // $table->string('email_perusahaan');
-            // $table->string('lokasi');
+
+            $table->string('jenis_izin');
+            $table->string('no_izin')->nullable();
+            $table->date('tanggal_izin_from');
+            $table->date('tanggal_izin_to');
+            $table->string('no_induk_usaha')->nullable();
 
 
-            // $table->string('file_denah_bangunan');
-            // $table->string('file_denah_lokasi');
-            // $table->string('file_izin_instansi');
-            // $table->string('file_surat_kuasa');
-            // $table->string('file_nib');
-            // $table->string('file_npwp_perusahaan');
-            // $table->string('file_ktp_pemilik');
-            // $table->string('file_npwp_pemilik');
-            // $table->string('file_surat_pernyataan');
-            // $table->string('file_data_registrasi');
+            $table->date('tanggal_kesiapan_cek_lokasi');
+
+            $table->string('file_denah_bangunan')->nullable();
+            $table->string('file_denah_lokasi')->nullable();
+            $table->string('file_izin_instansi')->nullable();
+            $table->string('file_surat_kuasa')->nullable();
+            $table->string('file_nib')->nullable();
+            $table->string('file_npwp_perusahaan')->nullable();
+            $table->string('file_ktp_pemilik')->nullable();
+            $table->string('file_npwp_pemilik')->nullable();
+            $table->string('file_surat_pernyataan')->nullable();
+            $table->string('file_data_registrasi')->nullable();
             
             $table->timestamps();
         });
