ef9f0d0bd468ea9d705de726c08cb582dd8b237f
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-06-24 16:23:30 +0700

    input form date

---
+++ resources/views/livewire/form/input-date.blade.php
@@ -0,0 +1,10 @@
+<div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+    <label for="inline-{{$name}}" class="font-bold mb-1 text-gray-700 block">{{$text}}</label>
+    </div>
+    <div class="md:w-2/3">
+        <input type="{{ $type ?? 'text' }}"
+            class="datepicker w-32 py-1 px-3 rounded-lg shadow-sm focus:outline-none focus:shadow-outline text-gray-600 font-medium"
+            placeholder="{{ $placeholder ?? 'Pilih '.$text.'...' }}">
+        </div>
+</div>
