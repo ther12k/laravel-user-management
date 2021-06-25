2d320cb11298f8674d5f59e3269425d5c8b0bf5e
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-06-25 15:54:39 +0700

    nppbkc pdf

--- app/Http/Controllers/NppbkcController.php
+++ app/Http/Controllers/NppbkcController.php
@@ -10,7 +10,26 @@ class NppbkcController extends Controller
     //
     public function permohonan_lokasi_pdf()
     {
-    	$pdf = PDF::loadview('pdf.permohonan_lokasi');
+    	$pdf = PDF::loadview('pdf.permohonan_lokasi')->setPaper('a4', 'potrait');
     	return $pdf->download('permohonan-lokasi-pdf');
     }
+
+    public function permohonan_nppbkc_pdf()
+    {
+    	$pdf = PDF::loadview('pdf.permohonan_nppbkc')->setPaper('a4', 'potrait');
+    	return $pdf->download('permohonan-nppbkc-pdf');
+    }
+
+    // public function permohonan_lokasi_pdf()
+    // {
+    //     // usersPdf is the view that includes the downloading content
+    //     $view = \View::make('pdf.permohonan_lokasi');
+    //     $html_content = $view->render();
+    //     // Set title in the PDF
+    //     PDF::SetTitle("NPPBKC");
+    //     PDF::AddPage();
+    //     PDF::writeHTML($html_content, true, false, true, false, '');
+    //     // userlist is the name of the PDF downloading
+    //     PDF::Output('permohonan_nppbkc.pdf');    
+    // }
 }
---
+++ resources/views/pdf/permohonan_nppbkc.blade.php
@@ -0,0 +1,63 @@
+<p style="text-align: center;"><strong>(NAMA USAHA)</strong></p>
+<p style="text-align: center;"><strong>(ALAMAT USAHA)</strong></p>
+<p style="padding-left: 30px;">Nomor : [NO PERMOHONAN]</p>
+<p style="padding-left: 30px;">Lampiran : 1 (Satu) Berkas</p>
+<p style="padding-left: 30px;">&nbsp;</p>
+<p style="padding-left: 30px;">Hal : Permohonan Mendapatkan Nomor<span lang="id-ID"> Pokok</span> PengusahaBarang Kena Cukai</p>
+<p style="padding-left: 30px;">(NPPBKC) sebagai(JENIS USAHA BKC)<span lang="id-ID"> Minuman Mengandung Etil Alkohol</span></p>
+<p style="padding-left: 30px;">&nbsp;</p>
+<p style="padding-left: 30px;">Yth. Menteri Keuangan Republik Indonesia</p>
+<p style="padding-left: 30px;">u.p. Kepala Kantor Bea dan Cukai Tipe Madya Pabean <span lang="id-ID">C Palangkaraya</span></p>
+<p style="padding-left: 30px;"><span lang="id-ID">di Palangka Raya</span></p>
+<p style="padding-left: 30px;">&nbsp;</p>
+<p style="padding-left: 30px;">Dengan hormat,</p>
+<p style="padding-left: 30px;">Yang bertanda tangan dibawah ini:</p>
+<p style="padding-left: 30px;"><span lang="id-ID">n</span>ama : (NAMA PEMOHON)</p>
+<p style="padding-left: 30px;"><span lang="id-ID">p</span>ekerjaan/<span lang="id-ID">j</span>abatan : (PEKERJAAN/JABATAN PEMOHON)</p>
+<p style="padding-left: 30px;"><span lang="id-ID">a</span>lamat : (ALAMAT PEMOHON)</p>
+<p style="padding-left: 30px;"><span lang="id-ID">n</span>omor telepon : (NO TLP PEMOHON)</p>
+<p style="padding-left: 30px;"><span lang="id-ID">a</span>lamat <span lang="id-ID">ponsel (</span><em>e-mail</em><span lang="id-ID">)</span> : (EMAIL PEMOHON)</p>
+<p style="padding-left: 30px;">&nbsp;</p>
+<p style="padding-left: 30px;">Bertindak atas nama:</p>
+<p style="padding-left: 30px;"><span lang="id-ID">n</span>ama <span lang="id-ID">p</span>emilik : (NAMA PEMILIK USAHA)</p>
+<p style="padding-left: 30px;"><span lang="id-ID">a</span>lamat <span lang="id-ID">p</span>emilik :(ALAMAT PEMILIK USAHA)</p>
+<p style="padding-left: 30px;">NPWP <span lang="id-ID">p</span>emilik : (NPWP USAHA)</p>
+<p style="padding-left: 30px;"><span lang="id-ID">n</span>omor <span lang="id-ID">t</span>elepon : (NO TLP USAHA)</p>
+<p style="padding-left: 30px;"><span lang="id-ID">a</span>lamat <span lang="id-ID">ponsel (</span><em>e-mail</em><span lang="id-ID">)</span> : (EMAIL USAHA)</p>
+<p style="padding-left: 30px;">&nbsp;</p>
+<p style="padding-left: 30px;">Mengajukan permohonan untuk mendapatkan NPPBKC sebagai <span lang="id-ID">Pengusaha </span>(JENIS USAHA BKC)<span lang="id-ID"> Barang Kena Cukai Berupa </span>Minuman Mengandung Etil Alkoholdengan rincian sebagai berikut:</p>
+<p style="padding-left: 30px;">&nbsp;</p>
+<p style="padding-left: 30px;"><span lang="id-ID">Perusahaan:</span></p>
+<p style="padding-left: 60px;">Lokasi (JENIS USAHA BKC)</p>
+<p style="padding-left: 60px;">Izin Usaha dari instansi terkait:&nbsp;</p>
+<p style="padding-left: 60px;">nama :(NAMA USAHA)</p>
+<p style="padding-left: 60px;">alamat : (ALAMAT USAHA)</p>
+<p style="padding-left: 60px;">NPWP :(NPWP USAHA)</p>
+<p style="padding-left: 60px;">nomor <span lang="id-ID">t</span>elepon : (NO TLP USAHA)</p>
+<p style="padding-left: 60px;">alamat <span lang="id-ID">ponsel (</span><em>e-mail</em><span lang="id-ID"><em>)</em></span> : (EMAIL USAHA)</p>
+<p style="padding-left: 30px;">&nbsp;</p>
+<p style="padding-left: 30px;">Lokasi (JENIS USAHA BKC) :</p>
+<p style="padding-left: 60px;"><span lang="id-ID">k</span>egunaan :(KEGUANAAN)</p>
+<p style="padding-left: 60px;"><span lang="id-ID">a</span>lamat :(ALAMAT USAHA)</p>
+<p style="padding-left: 60px;"><span lang="id-ID">k</span>elurahan/desa : (KELURAHAN USAHA)</p>
+<p style="padding-left: 60px;"><span lang="id-ID">k</span>ecamatan : (KECAMATAN USAHA)</p>
+<p style="padding-left: 60px;"><span lang="id-ID">k</span>abupaten/kota : (KABUPATEN USAHA)</p>
+<p style="padding-left: 60px;"><span lang="id-ID">p</span>rovinsi : (PROVINSI USAHA)</p>
+<p style="padding-left: 60px;"><span lang="id-ID">k</span>oordinat<span lang="id-ID">/geolokasi</span> : (KOORDINAT USAHA)</p>
+<p style="padding-left: 60px;">&nbsp;</p>
+<p style="padding-left: 30px;">Izin Usaha dari instansi terkait:</p>
+<p style="padding-left: 60px;">SIUP-MB SKMB : Nomor (NOMOR SIUP-MB/SKMB) tanggal (TANGGAL AWAL SIUP-MB/SKMB)</p>
+<p style="padding-left: 60px;">ITP-MB : Nomor (NOMOR ITP-MB) tanggal (TANGGAL AWAL ITP-MB)</p>
+<p style="padding-left: 60px;">NIB : Nomor (NOMOR NIB) tanggal (TANGGAL AWAL NIB)</p>
+<p style="padding-left: 30px;">Luas lokasi, luas bangunan, dan batas-batas lokasi yang akan dijadikan tempat usaha<span lang="id-ID"> s</span>ebagaimana tertera dalam berita acara pemeriksaan lokasi nomor (NOMOR BERITA ACARA CEK LOKASI)<span lang="id-ID">t</span>anggal(TANGGAL BERITA ACARA CEK LOKASI).</p>
+<p style="padding-left: 30px;">Lampiran-lampiran :</p>
+<p style="padding-left: 60px;"><span lang="id-ID">b</span>erita acara pemeriksaan lokasi;</p>
+<p style="padding-left: 60px;">salinan/fotocopy izin usaha dari instansi terkait;</p>
+<p style="padding-left: 60px;"><span lang="id-ID">l</span>ampiran lainnya.</p>
+<p style="padding-left: 60px;">&nbsp;</p>
+<p style="padding-left: 30px; text-align: right;">Dibuat di (KABUPATEN PEMOHONON)</p>
+<p style="padding-left: 30px; text-align: right;">Pada tanggal (TANGGAL PEGAJUAN)</p>
+<p style="padding-left: 30px; text-align: right;">Pemohon,</p>
+<p style="padding-left: 30px; text-align: right;">&nbsp;</p>
+<p style="padding-left: 30px; text-align: right;">&nbsp;</p>
+<p style="padding-left: 30px; text-align: right;"><strong>(NAMA PEMOHON)</strong></p>
