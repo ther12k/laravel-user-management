13db473a40578f516429afd362aecdda4df14795
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-06-25 15:55:10 +0700

    update form, preview

--- resources/views/livewire/wizard.blade.php
+++ resources/views/livewire/wizard.blade.php
@@ -32,7 +32,9 @@
 		<div x-show.transition="step != 'complete'">	
 			<!-- Top Navigation -->
 			<div class="border-b-2 py-4">
-				<div x-show="step !== 0 && step!==11" class="uppercase tracking-wide text-xs font-bold text-gray-500 mb-1 leading-tight" x-text="`Step: ${step} of 10`"></div>
+				<div x-show="step !== 0 && step!=='preview'" class="uppercase tracking-wide text-xs font-bold text-gray-500 mb-1 leading-tight" x-text="`Step: ${step} of 10`"></div>
+				<div x-show="step==='preview'" class="uppercase tracking-wide text-xs font-bold text-gray-500 mb-1 leading-tight">Preview Sebelum Kirim</div>
+				
 				<div class="flex flex-col md:flex-row md:items-center md:justify-between">
 					<div class="flex-1">
 						<div x-show="step === 0">
@@ -75,7 +77,7 @@
 						</div>
 					</div>
 
-					<div class="flex items-center md:w-64" x-show.transition="step !== 'complete' && step !== 0 && step!==11">
+					<div class="flex items-center md:w-64" x-show.transition="step !== 'complete' && step !== 0 && step!=='preview'">
 						
 						<div class="w-full bg-gray-300 rounded-full mr-2">
 							<div class="rounded-full bg-green-500 text-xs leading-none h-2 text-center text-white" :style="'width: '+ parseInt(step / 10 * 100) +'%'"></div>
@@ -94,7 +96,7 @@
 						@include('livewire.form.disclaimer')
 					</div>
 				</div>
-				<div x-show.transition.in="step === 1">
+				<div x-show.transition.in="step === 1||step == 'preview'">
 					<div class="md:flex md:items-center mb-6">
 						<div class="md:w-1/3">
 							<label for="status_pemohon" class="font-bold mb-1 text-gray-700 block">Status Permohonan</label>
@@ -129,9 +131,9 @@
 											])
 					@include('livewire.form.input',['type'=>'email','name'=>'email_pemilik','text'=>'Email Pemilik'])
 					
-					<div class="mb-6 py-5"></div>
+					<div class="mb-6 py-5" x-show="step===1"></div>
 				</div>
-				<div x-show.transition.in="step === 2">
+				<div x-show.transition.in="step === 2||step == 'preview'">
 					@include('livewire.form.select',['name'=>'jenis_usaha_bkc','text'=>'Jenis Usaha BKC',
 							'options'=>[
 								'Pengusaha Pabrik',
@@ -150,7 +152,7 @@
 							]
 						])
 				</div>
-				<div x-show.transition.in="step === 3">
+				<div x-show.transition.in="step === 3||step == 'preview'">
 					@include('livewire.form.input',['name'=>'nama_perusahaan','text'=>'Nama Usaha'])
 					@include('livewire.form.textarea',['name'=>'alamat_perusahaan','text'=>'Alamat Usaha'])
 					@include('livewire.form.input',['type'=>'number','name'=>'telp_perusahaan','text'=>'No Telp Usaha'])
@@ -162,7 +164,7 @@
 					@include('livewire.form.input',['type'=>'email','name'=>'email_perusahaan','text'=>'Email Usaha'])
 					
 				</div>
-				<div x-show.transition.in="step === 4">
+				<div x-show.transition.in="step === 4||step == 'preview'">
 
 					@include('livewire.form.select',['name'=>'jenis_lokasi','text'=>'Jenis Lokasi',
 							'options'=>[
@@ -185,7 +187,7 @@
 								]
 							])
 				</div>
-				<div x-show.transition.in="step === 5">
+				<div x-show.transition.in="step === 5||step == 'preview'">
 					@include('livewire.form.select',['name'=>'provinsi','text'=>'Provinsi',
 								'options'=>[
 									'DKI Jakarta',
@@ -211,20 +213,26 @@
 							])
 							
 				</div>
-				<div x-show.transition.in="step === 6">
+				<div x-show.transition.in="step === 6||step == 'preview'">
 					@include('livewire.form.input',['name'=>'rt_rw','text'=>'RT / RW'])
 					@include('livewire.form.textarea',['name'=>'alamat','text'=>'Alamat Lengkap'])
 					@include('livewire.form.input',['name'=>'lokasi_geo','text'=>'Koordinat Lokasi'])
 				</div>
-				<div x-show.transition.in="step === 7">
-					@include('livewire.form.input',['name'=>'no_induk_usaha','text'=>'No Induk Berusaha'])
-					@include('livewire.form.input-fromto-date',['name'=>'masa_berlaku_nik','text'=>'Tanggal masa berlaku'])
+				<div x-show.transition.in="step === 7||step == 'preview'">
+					@include('livewire.form.input',['name'=>'no_siup_mb','text'=>'Nomor Izin SIUP-MB / SKMB'])
+					@include('livewire.form.input-fromto-date',['name'=>'masa_berlaku_siup_mb','text'=>'Tanggal masa berlaku'])
+
+					@include('livewire.form.input',['name'=>'no_itp_mb','text'=>'Nomor Izin ITP-MB'])
+					@include('livewire.form.input-fromto-date',['name'=>'masa_berlaku_itp_mb','text'=>'Tanggal masa berlaku'])
+
+					@include('livewire.form.input',['name'=>'no_izin_nib','text'=>'No Izin NIB'])
+					@include('livewire.form.input-date',['name'=>'tanggal_nib','text'=>'Tanggal NIB'])
 				
 				</div>
-				<div x-show.transition.in="step === 8">
+				<div x-show.transition.in="step === 8||step == 'preview'">
 					@include('livewire.form.input-date',['name'=>'tanggal_kesiapan_cek_lokasi','text'=>'Tanggal kesiapan cek lokasi'])
 				</div>
-				<div x-show.transition.in="step === 9">
+				<div x-show.transition.in="step === 9||step == 'preview'">
 					<div class="md:flex md:items-center mb-6">
                         <div class="md:w-1/3">
                         <label for="inline-file_denah_bangunan" class="font-bold mb-1 text-gray-700 block">Denah di dalam Bangunan</label>
@@ -265,9 +273,10 @@
                             <x-file-attachment wire:model="file_nib" :file="$file_nib" />
                         </div>
                     </div>
+					<div class="mb-6 py-5" x-show="step===9"></div>
 				</div>
 
-				<div x-show.transition.in="step === 10">
+				<div x-show.transition.in="step === 10||step == 'preview'">
 					<div class="md:flex md:items-center mb-6">
                         <div class="md:w-1/3">
                         <label for="inline-file_npwp_perusahaan" class="font-bold mb-1 text-gray-700 block">NPWP Usaha</label>
@@ -308,6 +317,7 @@
                             <x-file-attachment wire:model="file_data_registrasi" :file="$file_data_registrasi" />
                         </div>
                     </div>
+					<div class="mb-6 py-5" x-show="step===10||step==='preview'"></div>
 				</div>
 
 				<div x-show.transition.in="step === 11">
@@ -337,13 +347,13 @@
 						class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium" 
 					>Next</button>
 					<button
-						@click="step++"
+						@click="step = 'preview'"
 						x-show="step === 10"
 						class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-gray-500 hover:bg-gray-600 font-medium" 
 					>Preview</button>
 					<button
 						@click="step = 'complete'"
-						x-show="step === 11"
+						x-show="step === 'preview'"
 						class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium" 
 					>Complete</button>
 				</div>
