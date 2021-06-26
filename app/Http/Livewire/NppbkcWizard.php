5d85b1a36e466334344d6cc51dc1a06da951fc19
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-06-26 15:04:29 +0700

    until state city select

--- app/Http/Livewire/NppbkcWizard.php
+++ app/Http/Livewire/NppbkcWizard.php
@@ -9,21 +9,71 @@ class NppbkcWizard extends Component
 {
     use WithFileUploads;
 
-    public $currentStep = 1;
-    public $status_pemohon='sendiri', $nama_pemilik, $alamat_pemilik, $status_nppbkc = 1;
+    public $step = 0, $status_nppbkc = 1;
+    public $status_pemohon='sendiri', $nama_pemilik, $alamat_pemilik,$email_pemilik,$telp_pemilik,$npwp_pemilik='xx.xxx.xxx.x-xxx.xxx';
+    public $jenis_usaha_bkc,$jenis_bkc; 
+    public $nama_usaha, $alamat_usaha,$email_usaha,$telp_usaha,$npwp_usaha='xx.xxx.xxx.x-xxx.xxx';
+    public $jenis_lokasi,$lokasi,$kegunaan; 
     public $file_denah_bangunan,$file_denah_lokasi,$file_izin_instansi,$file_surat_kuasa,$file_nib;
-    public $file_npwp_pemilik,$file_npwp_perusahaan,$file_ktp_pemilik,$file_surat_pernyataan,$file_data_registrasi;
+    public $file_npwp_pemilik,$file_npwp_usaha,$file_ktp_pemilik,$file_surat_pernyataan,$file_data_registrasi;
     public $successMessage = '';
 
-    protected $step1_rules = [
-        'status_pemohon' => 'required',
-        'nama_pemilik' => 'required',
-        'alamat_pemilik' => 'required'
+    protected $rules = [
+        [],
+        [
+            'status_pemohon' => 'required',
+            'nama_pemilik' => 'required|min:4',
+            'alamat_pemilik' => 'required|min:5',
+            'telp_pemilik' => 'required',
+            //xx.xxx.xxx.x-xxx.xxx
+            'npwp_pemilik' => 'required|regex:/^[0-9]{2}\.[0-9]{3}\.[0-9]{3}.[0-9]-[0-9]{3}\.[0-9]{3}$/',
+            'email_pemilik' => 'required|email:filter'
+        ],
+        [
+            'jenis_usaha_bkc' => 'required',
+            'jenis_bkc' => 'required',
+        ],
+        [
+            'nama_usaha' => 'required|min:4',
+            'alamat_usaha' => 'required',
+            'telp_usaha' => 'required',
+            //xx.xxx.xxx.x-xxx.xxx
+            'npwp_usaha' => 'required|regex:/^[0-9]{2}\.[0-9]{3}\.[0-9]{3}.[0-9]-[0-9]{3}\.[0-9]{3}$/',
+            'email_usaha' => 'required|email:filter'
+        ],
+        [
+            'jenis_lokasi' => 'required',
+            'lokasi' => 'required|min:5',
+            'kegunaan' => 'required'
+        ],
     ];
 
     protected $messages = [
-        'nama_pemilik.required' => 'Nama Pemilik tidak boleh kosong.',
-        'alamat_pemilik.required' => 'Alamat Pemilik tidak boleh kosong.'
+        'nama_pemilik.required' => 'Nama tidak boleh kosong.',
+        'alamat_pemilik.required' => 'Alamat tidak boleh kosong.',
+        'telp_pemilik.required' => 'No Telp tidak boleh kosong.',
+        'alamat_pemilik.required' => 'Alamat tidak boleh kosong.',
+        'npwp_pemilik.required' => 'NPWP tidak boleh kosong.',
+        'npwp_pemilik.regex' => 'NPWP tidak valid.',
+        'email_pemilik.required' => 'email tidak boleh kosong.',
+        'email_pemilik.email' => 'Format email salah.',
+
+        'jenis_usaha_bkc.required' => 'Pilih jenis usaha BKC.',
+        'jenis_bkc.required' => 'Pilih jenis BKC.',
+
+        'nama_usaha.required' => 'Nama tidak boleh kosong.',
+        'alamat_usaha.required' => 'Alamat tidak boleh kosong.',
+        'telp_usaha.required' => 'No Telp tidak boleh kosong.',
+        'alamat_usaha.required' => 'Alamat tidak boleh kosong.',
+        'npwp_usaha.required' => 'NPWP tidak boleh kosong.',
+        'npwp_usaha.regex' => 'NPWP tidak valid.',
+        'email_usaha.required' => 'email tidak boleh kosong.',
+        'email_usaha.email' => 'Format email salah.',
+
+        'jenis_lokasi.required' => 'Pilih jenis lokasi.',
+        'lokasi.required' => 'Lokasi tidak boleh kosong.',
+        'kegunaan.required' => 'Pilih kegunaan lokasi.',
+
     ];
 
 
@@ -47,19 +97,28 @@ class NppbkcWizard extends Component
 
     public function render()
     {
+
+
         return view('livewire.wizard')->extends('layouts.auth');
     }
-
+    
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName,$this->rules[$this->step]);
+    }
     /**
      * Write code on Method
      *
      * @return response()
      */
-    public function firstStepSubmit()
+    public function stepCheck()
     {   
-        $validatedData = $this->validate($this->step1_rules);
- 
-        $this->currentStep = 2;
+        if(0<$this->step&&$this->step<11){
+            $validatedData = $this->validate($this->rules[$this->step]);
+            $this->step++;
+        }else if($this->step==0){
+            $this->step=1;
+        }
     }
   
     /**
@@ -96,7 +155,7 @@ class NppbkcWizard extends Component
   
         $this->clearForm();
   
-        $this->currentStep = 1;
+        $this->step = 1;
     }
   
     /**
@@ -104,9 +163,9 @@ class NppbkcWizard extends Component
      *
      * @return response()
      */
-    public function back($step)
+    public function back()
     {
-        $this->currentStep = $step;    
+        $this->step--;    
     }
   
     /**
