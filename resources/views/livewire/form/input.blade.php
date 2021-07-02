e291fc22bf97a129499b11a75cb843e234939fe3
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-03 00:07:37 +0700

    exclamation icon on input

--- resources/views/livewire/form/input.blade.php
+++ resources/views/livewire/form/input.blade.php
@@ -4,19 +4,22 @@
     </div>
     <div class="md:w-2/3 flex"  x-data="{ tooltip: false }">
         @isset($tooltip)
-        <div class="relative" x-cloak x-show.transition.origin.top="tooltip">
-            <div class="absolute ml-40 lg:ml-14 top-0 z-10 w-64 p-2 px-4 -mt-1 text-sm leading-tight text-white transform -translate-x-1/2 -translate-y-full bg-blue-400 rounded-lg shadow-lg">
-                {{$tooltip}}
+        @endif  
+        @isset($tooltip)
+        <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-ful sm:mx-0 sm:h-10 sm:w-10" x-on:mouseover="tooltip = true" x-on:mouseleave="tooltip = false" >
+            <div class="relative" x-cloak x-show.transition.origin.top="tooltip">
+                <div class="absolute ml-50 lg:ml-14 top-0 z-10 w-64 p-2 px-4 -mt-4 text-sm leading-tight text-white transform -translate-x-4 lg:-translate-x-16 -translate-y-full bg-blue-400 rounded-lg shadow-lg">
+                    {{$tooltip}}
+                </div>
+                <svg class="absolute ml-11 lg:ml-14 z-10 w-6 h-6 -mt-3 text-blue-400 transform lg:-translate-x-16 -translate-x-12 -translate-y-3 fill-current stroke-current" width="8" height="8">
+                    <rect x="12" y="-10" width="8" height="8" transform="rotate(45)" />
+                </svg>
             </div>
-            <svg class="absolute ml-40 lg:ml-14 z-10 w-6 h-6 text-blue-400 transform -translate-x-12 -translate-y-3 fill-current stroke-current" width="8" height="8">
-                <rect x="12" y="-10" width="8" height="8" transform="rotate(45)" />
-            </svg>
+        <x-heroicon-o-exclamation class="h-6 w-6 text-red-600" 
+        />
         </div>
         @endif
-        <input         
-        @isset($tooltip)
-        x-on:mouseover="tooltip = true" x-on:mouseleave="tooltip = false" 
-        @endif
+        <input       
         name="{{$name}}"
         wire:model.defer="{{$name}}"
         type="{{ $type ?? 'text' }}"
