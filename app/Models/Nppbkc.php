9a2ad5b78d229876caae49f6be3fbf51a2fbfe70
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-02 20:27:39 +0700

    update model fillable and relation to user

--- app/Models/Nppbkc.php
+++ app/Models/Nppbkc.php
@@ -14,9 +14,11 @@ class Nppbkc extends Model
     use HasFactory,Blameable,LogsActivity,Notifiable;
     
     protected $fillable = [
+        'status_nppbkc',
         'status_pemohon','no_permohonan','nama_pemilik','alamat_pemilik','telp_pemilik','npwp_pemilik','email_pemilik',
         'jenis_usaha_bkc','jenis_bkc','nama_usaha','alamat_usaha','telp_usaha','npwp_usaha','email_usaha',
         'jenis_lokasi','lokasi','kegunaan','village_id','rt_rw','alamat',
+        'lokasi_latitude','lokasi_longitude',
         'province_id','regency_id','district_id','village_id',
         'no_siup_mb','masa_berlaku_siup_mb_from','masa_berlaku_siup_mb_to','no_itp_mb',
         'masa_berlaku_itp_mb_from','masa_berlaku_itp_mb_to','no_izin_nib',
@@ -41,10 +43,16 @@ class Nppbkc extends Model
         return "NPPBKC has been {$eventName}";
     }
 
+    public function createdBy()
+    {
+        return $this->hasMany(User::class,'id','created_by');
+    }
+
     public function files()
     {
         return $this->hasMany(NppbkcFile::class);
     }
+
     public function province()
     {
         return $this->belongsTo(Province::class);
@@ -57,6 +65,7 @@ class Nppbkc extends Model
     {
         return $this->belongsTo(District::class);
     }
+
     public function village()
     {
         return $this->belongsTo(Village::class);
