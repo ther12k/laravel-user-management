53cdef7773b63dbf2ac20f43755725b66f894be4
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-02 20:28:08 +0700

    view nppbkc fix

--- app/Http/Livewire/ViewNppbkc.php
+++ app/Http/Livewire/ViewNppbkc.php
@@ -4,18 +4,38 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Nppbkc;
+use Auth;
 
 class ViewNppbkc extends Component
 {
     public $newid;
-    public function render()
-    {
-        $nppbkc = Nppbkc::findOrFail($this->newid);
-        return view('livewire.nppbkc',$nppbkc->toArray());
-    }
+
     public function mount($id)
     {
-        $nppbkc = Nppbkc::findOrFail($id);
-        $this->newid = $nppbkc->id;
+        $this->newid = $id;
+    }
+    public function render()
+    {
+        $nppbkc = Nppbkc::find($this->newid);
+        if($nppbkc==null) abort(404);
+        if(!\Gate::allows('viewAllNppbkc')){
+            if($nppbkc->created_by!=Auth::user()->id){
+                abort(401);
+            }
+        }
+        
+        $data = array_merge($nppbkc->toArray(),
+            [
+                'files'=>$nppbkc->files->all()
+            ]
+        );
+        
+        return view('livewire.nppbkc',$data);
+    }
+
+
+    public function openmap()
+    {
+        $this->newid = $id;
     }
 }
