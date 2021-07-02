91d6fe773ec9a71594e31e49dec22e9d14c028d1
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-03 00:53:29 +0700

    refactor modal

--- resources/views/livewire/nppbkc.blade.php
+++ resources/views/livewire/nppbkc.blade.php
@@ -211,61 +211,35 @@ x-data="{
 		<!-- / Bottom Navigation https://placehold.co/300x300/e2e8f0/cccccc -->	
 
 		@if($isOpen)
-		<div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-			<div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-			<!--
-				Background overlay, show/hide based on modal state.
-		
-				Entering: "ease-out duration-300"
-				From: "opacity-0"
-				To: "opacity-100"
-				Leaving: "ease-in duration-200"
-				From: "opacity-100"
-				To: "opacity-0"
-			-->
-			<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-		
-			<!-- This element is to trick the browser into centering the modal contents. -->
-			<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-		
-			<!--
-				Modal panel, show/hide based on modal state.
-		
-				Entering: "ease-out duration-300"
-				From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-				To: "opacity-100 translate-y-0 sm:scale-100"
-				Leaving: "ease-in duration-200"
-				From: "opacity-100 translate-y-0 sm:scale-100"
-				To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-			-->
-			<div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+		<x-nppbkc-modal>
+            <x-slot name="content">
 				<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-				<div class="sm:flex sm:items-start">
-					<div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-					<!-- Heroicon name: outline/exclamation -->
-					<x-heroicon-o-exclamation class="h-6 w-6 text-indigo-600"/>
-					</div>
-					<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-						<div class="mt-2">
-							<h1>Persetujuan Cek Lokasi</h1>
-							<p class="text-sm text-gray-500">
-							Apakah Permohonan ini akan disetujui? (pastikan data sudah lengkap)
-							</p>
+					<div class="sm:flex sm:items-start">
+						<div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
+						<!-- Heroicon name: outline/exclamation -->
+						<x-heroicon-o-exclamation class="h-6 w-6 text-indigo-600"/>
+						</div>
+						<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+							<div class="mt-2">
+								<h1>Persetujuan Cek Lokasi</h1>
+								<p class="text-sm text-gray-500">
+								Apakah Permohonan ini akan disetujui? (pastikan data sudah lengkap)
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
-				</div>
 				<div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-				<button  wire:click.prevent="setuju_cek()" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
-					Setuju
-				</button>
-				<button wire:click="closeModal()" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-					Cancel
-				</button>
+					<button  wire:click.prevent="setuju_cek()" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
+						Setuju
+					</button>
+					<button wire:click="closeModal()" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+						Cancel
+					</button>
 				</div>
-			</div>
-			</div>
-		</div>
+            </x-slot>
+        </x-nppbkc-modal>
+		
     @endif
 </div>
 @push('script')
