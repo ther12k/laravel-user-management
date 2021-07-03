f7bbaa41b5ffbccd3403d2507562ac045da6be91
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-03 15:59:19 +0700

    update view nppbkc

--- app/Http/Livewire/ViewNppbkc.php
+++ app/Http/Livewire/ViewNppbkc.php
@@ -3,13 +3,26 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use App\Models\Nppbkc;
 use Auth;
 
 class ViewNppbkc extends Component
 {
+    use WithFileUploads;
     public $nppbkc_id;
     public $isOpen=false;
+    public $catatan_petugas;
+    public $file_surat_tugas,$file_ba_periksa,$file_ba_wawancara;
+    public $activeTab=1;
+    public $inactiveClass='hover:text-purple-600 bg-gray-300 text-gray-600 bg-white text-xs font-bold uppercase px-5 py-3 block leading-normal';
+    public $activeClass='text-purple-600 bg-white text-xs font-bold uppercase px-5 py-3 block leading-normal';
+
+    public $petugas_files=[
+        'file_surat_tugas'=>'Surat tugas periksa lokasi',
+        'file_ba_periksa'=>'Berita acara periksa lokasi',
+        'file_ba_wawancara'=>'Berita acara wawancara'
+    ];
 
     public function mount($id)
     {
@@ -24,6 +37,8 @@ class ViewNppbkc extends Component
     public function openModal()
     {
         $this->isOpen = true;
+        $this->resetErrorBag();
+        $this->resetValidation();
     }
 
     /**
@@ -34,7 +49,9 @@ class ViewNppbkc extends Component
     public function closeModal()
     {
         $this->isOpen = false;
+        $this->dispatchBrowserEvent('showMap');
     }
+    
     public function render()
     {
         $nppbkc = Nppbkc::findOrFail($this->nppbkc_id);
@@ -47,7 +64,8 @@ class ViewNppbkc extends Component
         
         $data = array_merge($nppbkc->toArray(),
             [
-                'files'=>$nppbkc->files->all()
+                'files'=>$nppbkc->files->all(),
+                'petugas_files'=>$this->petugas_files
             ]
         );
         
--- resources/views/livewire/nppbkc.blade.php
+++ resources/views/livewire/nppbkc.blade.php
@@ -8,92 +8,92 @@
 </style>
 @endpush    
 <div x-cloak class="md:flex no-wrap md:-mx-2" id="tabs-id"
-x-data="{ 
-    activeTab:1,
-    inactiveClass: 'hover:text-purple-600 bg-gray-300 text-gray-600 bg-white text-xs font-bold uppercase px-5 py-3 block leading-normal',
-    activeClass : 'text-purple-600 bg-white text-xs font-bold uppercase px-5 py-3 block leading-normal'
- }" 
+x-data="app()" 
 >
 
 <div class="w-full md:w-9/12 mx-auto">
-		<div class="mb-4 bg-white px-4 py-5">
-			@if (session()->has('message'))
-			<div id="alert" class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-green-400">
-				<span class="inline-block align-middle mr-8">
-					{{ session('message') }}
-				</span>
-				<button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none" onclick="document.getElementById('alert').remove();">
-					<span>×</span>
-				</button>
-			</div>
-			@endif
-			<div class="tab-content tab-space flex">
-				<div x-show="true" class="w-2/3">
-					<h1 class="flex-auto text-xl font-semibold text-purple-700 mb-4">Permohonan Pemeriksaan Lokasi </h1>
-					<h2 class="flex-auto text-lg font-semibold">No {{$no_permohonan}}</h2>
-					<h2>Tanggal Kesiapan Cek :  {{$tanggal_kesiapan_cek_lokasi}}</h2>
-					<div class="flex" >
-						<div><span>Status : </span> </div>
-						
-						<div class="hover:text-indigo-600 flex cursor-pointer" wire:click="openModal()">
-							@include('nppbkc.status',['status'=>$status_nppbkc]) <x-heroicon-o-pencil-alt class="h-6 w-6" />
-						</div>
-					</div>
-						{{-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-					</svg>  --}}
-					@can('updateNppbkc')
-					<button class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium" 
-					>Proses</button>
-					@endcan
-				</div>
-				<div class="w-1/3">
+
+	@if(!$isOpen||$status_nppbkc!=2)
+	<div class="mb-4 bg-white px-4 py-5">
+		@if (session()->has('message'))
+		<div id="alert" class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-green-400">
+			<span class="inline-block align-middle mr-8">
+				{{ session('message') }}
+			</span>
+			<button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none" onclick="document.getElementById('alert').remove();">
+				<span>×</span>
+			</button>
+		</div>
+		@endif
+		<div class="tab-content tab-space flex">
+			<div x-show="true" class="w-2/3">
+				<h1 class="flex-auto text-xl font-semibold text-purple-700 mb-4">Permohonan Pemeriksaan Lokasi / NPPBKC </h1>
+				<h2 class="flex-auto text-lg font-semibold">No {{$no_permohonan}}</h2>
+				<h2>Tanggal Kesiapan Cek :  {{$tanggal_kesiapan_cek_lokasi}}</h2>
+				<div class="flex" >
+					<div><span>Status : </span> </div>
 					
+					<div class="hover:text-indigo-600 flex cursor-pointer" wire:click="openModal()">
+						@include('nppbkc.status',['status'=>$status_nppbkc]) <x-heroicon-o-pencil-alt class="h-6 w-6" />
+					</div>
 				</div>
+					{{-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+				</svg>  --}}
+				@can('updateNppbkc')
+				<button class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium" 
+				>Proses</button>
+				@endcan
+			</div>
+			<div class="w-1/3">
+				
 			</div>
 		</div>
-		<div class="flex space-x-2">
-			<a href="#content" @click="activeTab = 1" :class="activeTab === 1 ? activeClass : inactiveClass">	
-				<div class="flex items-center space-x-2 font-semibold leading-8">
-					<span clas="text-green-500">
-						<x-heroicon-o-user class="h-6 w-6"/>
-					</span>
-					<span class="hidden lg:block">Data Pemohon</span>
-				</div>
-			</a>
-			<a href="#content" @click="activeTab = 2" :class="activeTab === 2 ? activeClass : inactiveClass">
-				<div class="flex items-center space-x-2 font-semibold leading-8">
-					<span clas="text-green-500">
-						<x-heroicon-o-office-building class="h-6 w-6"/>
-					</span>
-					<span class="hidden lg:block">Data Usaha</span>
-				</div>	
-			</a>
-			<a href="#content" @click="activeTab = 3" :class="activeTab === 3 ? activeClass : inactiveClass">
-				<div class="flex items-center space-x-2 font-semibold leading-8">
-					<x-heroicon-o-location-marker class="h-6 w-6"/>
-					</span><span class="hidden lg:block">Lokasi</span>
-				</div>
-			</a>
-			<a href="#content" @click="activeTab = 4" :class="activeTab === 4 ? activeClass : inactiveClass">
-				<div class="flex items-center space-x-2 font-semibold leading-8">
-					<span clas="text-green-500">
-						<x-heroicon-o-briefcase class="h-6 w-6"/>
-					</span>
-					<span class="hidden lg:block">Izin Usaha</span>
-				</div>
-			</a>
-			<a href="#content" @click="activeTab = 5" :class="activeTab === 5 ? activeClass : inactiveClass">
-				<div class="flex items-center space-x-2 font-semibold leading-8">
-					<span clas="text-green-500">
-						<x-heroicon-o-paper-clip class="h-6 w-6"/>
-					</span>
-					<span class="hidden lg:block">Lampiran</span>
-				</div>
-			</a>
-		</div>
-		<div class="bg-white mb-6 shadow-lg">
-			<div class="px-4 py-5">
+	</div>
+	<div class="flex space-x-2">
+		<a href="#content" @click="activeTab = 1" :class="activeTab === 1 ? activeClass : inactiveClass">	
+			<div class="flex items-center space-x-2 font-semibold leading-8">
+				<span clas="text-green-500">
+					<x-heroicon-o-user class="h-6 w-6"/>
+				</span>
+				<span class="hidden lg:block">Data Pemohon</span>
+			</div>
+		</a>
+		<a href="#content" @click="activeTab = 2" :class="activeTab === 2 ? activeClass : inactiveClass">
+			<div class="flex items-center space-x-2 font-semibold leading-8">
+				<span clas="text-green-500">
+					<x-heroicon-o-office-building class="h-6 w-6"/>
+				</span>
+				<span class="hidden lg:block">Data Usaha</span>
+			</div>	
+		</a>
+		<a href="#content" @click="activeTab = 3" :class="activeTab === 3 ? activeClass : inactiveClass">
+			<div class="flex items-center space-x-2 font-semibold leading-8">
+				<x-heroicon-o-location-marker class="h-6 w-6"/>
+				</span><span class="hidden lg:block">Lokasi</span>
+			</div>
+		</a>
+		<a href="#content" @click="activeTab = 4" :class="activeTab === 4 ? activeClass : inactiveClass">
+			<div class="flex items-center space-x-2 font-semibold leading-8">
+				<span clas="text-green-500">
+					<x-heroicon-o-briefcase class="h-6 w-6"/>
+				</span>
+				<span class="hidden lg:block">Izin Usaha</span>
+			</div>
+		</a>
+		<a href="#content" @click="activeTab = 5" :class="activeTab === 5 ? activeClass : inactiveClass">
+			<div class="flex items-center space-x-2 font-semibold leading-8">
+				<span clas="text-green-500">
+					<x-heroicon-o-paper-clip class="h-6 w-6"/>
+				</span>
+				<span class="hidden lg:block">Lampiran</span>
+			</div>
+		</a>
+	</div>
+	@endif
+	<div class="bg-white mb-6 shadow-lg">
+		<div class="px-4 py-5">
+			@if(!$isOpen||$status_nppbkc!=2)
 				<div x-show="activeTab === 1">
 					<div class="lg:hidden items-center space-x-2 font-semibold text-gray-900 leading-8">
 						<span class="tracking-wide">Data Pemohon {{ $status_pemohon=='dikuasakan' ?'(Dikuasakan)':''}}</span>
@@ -137,7 +137,7 @@ x-data="{
 							<div class="flex items-center hover:opacity-75 mr-4 px-4">
 								<i class="mr-2"><br>
 									<x-heroicon-o-map class="h-6 w-6 text-indigo-500"/>
-								  <br>
+								<br>
 										</i><p></p>
 								<a href="http://maps.google.com/maps?q={{$lokasi_latitude}},{{$lokasi_longitude}}" target="_blank" class="mt-1 text-indigo-500 font-bold">Open via google map</a>
 							</div>
@@ -186,33 +186,64 @@ x-data="{
 						@endforeach
 					</div>
 				</div>
-			</div>
-		</div>
-		<div class="h-1"></div>
-		<!-- Bottom Navigation -->	
-		<div class="sm:hidden fixed bottom-0 z-50 left-0 right-0 py-5 bg-white shadow-md">
-			<div class="max-w-3xl mx-auto px-4">
-				<div class="flex justify-between">
-					<div class="w-1/2">
-						<button
-							wire:click="back()"
-							class="w-32 focus:outline-none py-2 px-5 rounded-lg shadow-sm text-center text-white bg-red-500 hover:bg-red-600 font-medium border" 
-						>Kembali</button>
-					</div>
+			@endif
+			@if($isOpen&&$status_nppbkc==2)
+			<div>
+				<div class="p-10">
+					<h1 class="flex-auto text-xl font-semibold text-purple-700 mb-4">Penerimaan Permohonan Penerbitan NPPBKC</h1>
+					
+					@include('livewire.form.textarea',['name'=>'catatan_petugas','text'=>'Catatan Petugas'])
+					@foreach ($petugas_files as $name=>$text)
 
-					<div class="w-1/2 text-right">
-						<button
-							class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium" 
-						>Proses</button>
-					</div>
+						{{-- @include('livewire.form.upload',['name'=>$name,'text'=>$text]) --}}
+						<div class="md:flex md:items-center mb-6">
+							<div>
+								<label for="{{$name}}" class="font-bold mb-1 text-gray-700 block">{{$text}}</label>
+							
+								<x-file-attachment class="@error($name) border-red-500 @enderror " wire:model="{{$name}}" 
+								:file="${$name}" :preview-h="12" :preview_w="24"/>
+								@error($name) 
+								<span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+									{{ $message }}
+								</span> 
+								@enderror
+							</div>
+						</div>
+					@endforeach
+					<div class="h-4"></div>
+				</div>
+			</div>
+			@endif
+		</div>
+	</div>
+	<div class="h-1"></div>
+	@if($isOpen&&$status_nppbkc==2)
+	<!-- Bottom Navigation -->	
+	<div class="fixed bottom-0 z-50 left-0 right-0 py-5 bg-white shadow-md">
+		<div class="max-w-3xl mx-auto px-4">
+			<div class="flex justify-between">
+				<div class="w-1/2">
+					<button
+						wire:click="closeModal()"
+						class="w-32 focus:outline-none py-2 px-5 rounded-lg shadow-sm text-center text-white bg-red-500 hover:bg-red-600 font-medium border" 
+					>Kembali</button>
+				</div>
+
+				<div class="w-1/2 text-right">
+					<button
+						class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium" 
+					>Proses</button>
 				</div>
 			</div>
 		</div>
-		<!-- / Bottom Navigation https://placehold.co/300x300/e2e8f0/cccccc -->	
-
-		@if($isOpen)
+	</div>
+	<!-- / Bottom Navigation https://placehold.co/300x300/e2e8f0/cccccc -->	
+	@endif
+	
+	@if($isOpen)
+		@if($status_nppbkc==1)
 		<x-nppbkc-modal>
-            <x-slot name="content">
+			<x-slot name="content">
 				<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
 					<div class="sm:flex sm:items-start">
 						<div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -237,10 +268,10 @@ x-data="{
 						Cancel
 					</button>
 				</div>
-            </x-slot>
-        </x-nppbkc-modal>
-		
-    @endif
+			</x-slot>
+		</x-nppbkc-modal>
+		@endif
+	@endif
 </div>
 @push('script')
 
@@ -253,11 +284,24 @@ x-data="{
 	var defaultLocation;
 	var loaded = false;
 	var skeleton;
+	var activeTab = 1;
+	function app(){
+		return{
+			inactiveClass: 'hover:text-purple-600 bg-gray-300 text-gray-600 text-xs font-bold uppercase px-5 py-3 block leading-normal',
+			activeClass :'text-purple-600 bg-white text-xs font-bold uppercase px-5 py-3 block leading-normal',
+			activeTab :1
+		}
+	}
 
 	document.addEventListener('livewire:load', function () {
 		defaultLocation =  [{{$lokasi_longitude}}, {{$lokasi_latitude}}];
 		loadPreviewMap();
 	})
+
+	window.addEventListener('showMap', event => {
+		defaultLocation =  [{{$lokasi_longitude}}, {{$lokasi_latitude}}];
+		loadPreviewMap();
+	});
 
 	// 	flatpickr.localize(flatpickr.l10ns.id);
 	// 	flatpickr('.datepicker',{
