a5a77f63ad5b84741e3895f288ab6420d4e33daa
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-04 00:38:45 +0700

    use header dark

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,12 @@
 
 @section('body')
 <x-header-dark/>
+{{-- <header class="bg-white shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        Header
+    </div>
+</header> --}}
 <div class="md:flex flex-col md:flex-row md:min-h-screen w-full" id="content">
-    
     @yield('content')
     
     @isset($slot)
