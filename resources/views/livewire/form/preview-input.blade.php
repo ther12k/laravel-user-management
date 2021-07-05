4ebc090c761630565b7d0d89028c7d63b42ebda9
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-06 01:11:15 +0700

    input and download preview updated

--- resources/views/livewire/form/preview-input.blade.php
+++ resources/views/livewire/form/preview-input.blade.php
@@ -1,4 +1,4 @@
 <div class="grid grid-cols-2">
-    <div class="px-4 py-2 font-semibold">{{$text}}</div>
-    <div class="px-4 py-2">{{ ${$name} }}</div>
+    <div class="px-4 py-2 font-semibold flex-grow">{{$text}}</div>
+    <div class="px-4 py-2 flex-grow">{{ ${$name} }}</div>
 </div>            
