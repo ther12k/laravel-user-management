641346b9331a054ecea6aff92b56aa95d808030e
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-06 01:12:09 +0700

    annotation belongs to

--- app/Models/NppbkcAnnotation.php
+++ app/Models/NppbkcAnnotation.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Nppbkc;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use DigitalCloud\Blameable\Traits\Blameable;
@@ -13,5 +15,9 @@ class NppbkcAnnotation extends Model
     public function scopeOfStatus($query,$status_nppbkc)
     {
         return $query->where('status_nppbkc', $status_nppbkc);
+    }
+    public function nppbkc()
+    {
+        return $this->belongsTo(Nppbkc::class);
     }
 }
