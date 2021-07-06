e19d1d0f8a7852b27e9d1f6337fff57f54a1920d
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-06 22:27:45 +0700

    update-profile add file

--- resources/views/livewire/update-profile.blade.php
+++ resources/views/livewire/update-profile.blade.php
@@ -1,6 +1,13 @@
 @section('title', 'Update Profile')
+@push('styles')
+<style>
+	[x-cloak] {
+		display: none;
+	}
 
-<div x-data="app()" x-cloak>
+</style>
+@endpush   
+<div>
     <div>
         @if(session()->has('message'))
             <div class="alert alert-success">
@@ -8,7 +15,7 @@
             </div>
         @endif
     </div>
-	<div x-data="app()" x-cloak>
+	<div>
         <div class="max-w-3xl mx-auto">
             <!-- Top Navigation -->
 			<div class="border-b-2 py-4">
@@ -21,17 +28,19 @@
 			</div>
 			<!-- /Top Navigation -->
             <!-- Bottom Navigation -->	
-            <div class="fixed bottom-0 left-0 right-0 py-5 bg-white shadow-md" x-show="step != 'complete'">
+            <div class="fixed bottom-0 left-0 right-0 py-5 bg-white shadow-md z-50" x-show="step != 'complete'">
                 <div class="max-w-3xl mx-auto px-4">
                     <div class="flex justify-between">
                         <div class="w-1/2">
                             <button
+								wire:click="logout"
                                 class="w-32 focus:outline-none py-2 px-5 rounded-lg shadow-sm text-center text-gray-600 bg-white hover:bg-gray-100 font-medium border" 
                             >Log out</button>
                         </div>
 
                         <div class="w-1/2 text-right">
                             <button
+								wire:click="update"
                                 class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium" 
                             >Save</button>
                         </div>
@@ -42,9 +51,10 @@
             <!-- Content -->
 			<div class="py-10">	
 				<div>
-					@include('livewire.form.input',['name'=>'pekerjaan','text'=>'Pekerjaan'])
-					@include('livewire.form.textarea',['name'=>'alamat','text'=>'Alamat'])
-					@include('livewire.form.input',['type'=>'number','name'=>'no_telp','text'=>'No Telp'])
+					@include('livewire.form.input',['name'=>'nama','text'=>'Nama','nama'=>$nama])
+					@include('livewire.form.input',['name'=>'pekerjaan','text'=>'Pekerjaan','pekerjaan'=>$pekerjaan])
+					@include('livewire.form.textarea',['name'=>'alamat','text'=>'Alamat','alamat'=>$alamat])
+					@include('livewire.form.input',['type'=>'number','name'=>'no_telp','text'=>'No Telp','no_telp'=>$no_telp])
                     {{-- <div class="md:flex md:items-center mb-6">
                         <div class="md:w-1/3">
                         <label for="inline-npwp" class="font-bold mb-1 text-gray-700 block">NPWP</label>
@@ -63,14 +73,23 @@
                     </div> --}}
 					<div class="grid md:grid-cols-2 mb-6">
                         <div class="p-2">
-                        <label for="inline-npwp_photo" class="font-bold mb-1 text-gray-700 block">NPWP</label>
-                            <x-file-attachment wire:model="npwp_photo" :file="$npwp_photo" />
-							@include('livewire.form.error-span',['name'=>'npwp_photo'])
+                        <label for="inline-file_npwp" class="font-bold mb-1 text-gray-700 block">NPWP</label>
+                            <x-file-attachment wire:model="file_npwp" :file="$file_npwp" />
+							@include('livewire.form.error-span',['name'=>'file_npwp'])
                         </div>
                         <div class="p-2">
-                        <label for="inline-ktp_photo" class="font-bold mb-1 text-gray-700 block">KTP</label>
-                            <x-file-attachment wire:model="ktp_photo" :file="$ktp_photo" />
-							@include('livewire.form.error-span',['name'=>'ktp_photo'])
+                        <label for="inline-file_ktp" class="font-bold mb-1 text-gray-700 block">KTP</label>
+                            <x-file-attachment wire:model="file_ktp" :file="$file_ktp" />
+							@include('livewire.form.error-span',['name'=>'file_ktp'])
+                        </div>
+
+						
+                    </div>
+					<div class="grid md:grid-cols-2 mb-6">
+                        <div class="p-2">
+                        <label for="inline-file_registrasi_pengusaha_bkc" class="font-bold mb-1 text-gray-700 block">Data Registrasi Pengusaha BKC (Download format registrasi di <a href="">Link</a>)</label>
+                            <x-file-attachment wire:model="file_registrasi_pengusaha_bkc" :file="$file_registrasi_pengusaha_bkc" />
+							@include('livewire.form.error-span',['name'=>'file_registrasi_pengusaha_bkc'])
                         </div>
                     </div>
 				</div>
@@ -79,55 +98,3 @@
         </div>
     </div>	
 </div>
-
-<script>
-	function doFormat(x, pattern, mask) {
-		var strippedValue = x.replace(/[^0-9]/g, "");
-		var chars = strippedValue.split('');
-		var count = 0;
-
-		var formatted = '';
-		for (var i=0; i<pattern.length; i++) {
-			const c = pattern[i];
-			if (chars[count]) {
-			if (/\*/.test(c)) {
-				formatted += chars[count];
-				count++;
-			} else {
-				formatted += c;
-			}
-			} else if (mask) {
-			if (mask.split('')[i])
-				formatted += mask.split('')[i];
-			}
-		}
-		return formatted;
-	}
-	function app() {
-		document.querySelectorAll('[data-mask]').forEach(function(e) {
-		function format(elem) {
-			const val = doFormat(elem.value, elem.getAttribute('data-format'));
-			elem.value = doFormat(elem.value, elem.getAttribute('data-format'), elem.getAttribute('data-mask'));
-			
-			if (elem.createTextRange) {
-			var range = elem.createTextRange();
-			range.move('character', val.length);
-			range.select();
-			} else if (elem.selectionStart) {
-			elem.focus();
-			elem.setSelectionRange(val.length, val.length);
-			}
-		}
-		e.addEventListener('keyup', function() {
-			format(e);
-		});
-		e.addEventListener('keydown', function() {
-			format(e);
-		});
-		format(e)
-		});
-		return {
-			step: 0, 
-		}
-	}
-</script>
