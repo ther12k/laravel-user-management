920b847c2a3e399fc59a7f8160d121faefa28029
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-06 22:23:30 +0700

    profile middleware

---
+++ app/Http/Middleware/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Auth;
+class Profile
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+        if (!$user->hasProfile) {
+            return redirect()->route('user.profile');
+        }
+        
+        return $next($request);
+    }
+}
