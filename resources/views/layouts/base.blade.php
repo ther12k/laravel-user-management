be42f81fd2d16a19c9f86f54539715b55bcacc23
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-08 23:06:45 +0700

    change favico

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,7 +11,10 @@
         @else
             <title>{{ config('app.name') }}</title>
         @endif
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
 
         <!-- Scripts -->
         <script src="{{ url(mix('js/app.js')) }}" defer></script>
