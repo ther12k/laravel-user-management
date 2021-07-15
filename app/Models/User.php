209143292be775eb1982db67c0f1675c8983f1f3
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-15 10:37:17 +0700

    user profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\MailResetPasswordNotification;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -47,4 +48,34 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function getHasProfileAttribute()
+    {
+      return $this->profile_id !== null;
+    }
+    public function getHasAdminProfileAttribute()
+    {
+      return $this->profile_type == 'App\AdminProfile';
+    }
+    public function getHasUserProfileAttribute()
+    {
+      return $this->profile_type == 'App\Models\UserProfile';
+    }
+    public function getHasOfficerProfileAttribute()
+    {
+      return $this->profile_type == 'App\OfficerProfile';
+    }
+    public function files()
+    {
+        return $this->hasMany(UserFile::class);
+    }
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    // public function sendPasswordResetNotification($token)
+    // {
+    //     $this->notify(new MailResetPasswordNotification($token));
+    // }
 }
