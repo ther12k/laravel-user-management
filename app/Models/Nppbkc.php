778bbec917e9bef162e473471de8d9a188753248
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-15 11:49:10 +0700

    logging

--- app/Models/Nppbkc.php
+++ app/Models/Nppbkc.php
@@ -26,13 +26,17 @@ class Nppbkc extends Model
     ];
 
     //protected static $recordEvents = ['created','updated','deleted'];
-    protected static $logAttributes = ['created_by', 'updated_by','status_pemohon','nama_pemilik','alamat_pemilik','telp_pemilik','npwp_pemilik','email_pemilik',
-    'jenis_usaha_bkc','jenis_bkc','nama_usaha','alamat_usaha','telp_usaha','npwp_usaha','email_usaha',
-    'jenis_lokasi','lokasi','kegunaan','village_id','rt_rw','alamat',
-    'province_id','regency_id','district_id','village_id',
-    'no_siup_mb','masa_berlaku_siup_mb_from','masa_berlaku_siup_mb_to','no_itp_mb',
-    'masa_berlaku_itp_mb_from','masa_berlaku_itp_mb_to','no_izin_nib',
-    'tanggal_nib','tanggal_kesiapan_cek_lokasi'
+    protected static $logAttributes = [
+        'status_nppbkc','created_by', 'updated_by',
+        'no_permohonan_lokasi','catatan_petugas',
+        'status_pemohon','no_permohonan','nama_pemilik','alamat_pemilik','telp_pemilik','npwp_pemilik','email_pemilik',
+        'jenis_usaha_bkc','jenis_bkc','nama_usaha','alamat_usaha','telp_usaha','npwp_usaha','email_usaha',
+        'jenis_lokasi','kegunaan','village_id','rt_rw','alamat',
+        'lokasi_latitude','lokasi_longitude',
+        'province_id','regency_id','district_id','village_id',
+        'no_siup_mb','masa_berlaku_siup_mb_from','masa_berlaku_siup_mb_to','no_itp_mb',
+        'masa_berlaku_itp_mb_from','masa_berlaku_itp_mb_to','no_izin_nib',
+        'tanggal_nib','tanggal_kesiapan_cek_lokasi'
     ];
     protected static $submitEmptyLogs = false;
     protected static $logOnlyDirty = true;
