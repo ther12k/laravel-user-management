23b2019f0678cb6d0513d983a00583c08be3436b
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-16 15:40:05 +0700

    download file fix

--- resources/views/livewire/nppbkc/annotation/view.blade.php
+++ resources/views/livewire/nppbkc/annotation/view.blade.php
@@ -10,7 +10,7 @@
     <div class="grid md:grid-cols-2 text-sm mt-4">
         @foreach ($files as $file )
         <div class="px-4 py-2 font-semibold flex">
-            <x-heroicon-o-link class="h-6 w-6 mr-2 p-1"/><a href="{{route('nppbkc.downloadfile',['id'=>$file->id])}}" class="text-indigo-500 hover:text-indigo-700">{{$file->title}}</a>
+            <x-heroicon-o-link class="h-6 w-6 mr-2 p-1"/><a href="{{route('nppbkc.download-file',['id'=>$file->key])}}" class="text-indigo-500 hover:text-indigo-700">{{$file->title}}</a>
         </div>
         @endforeach
     </div>
