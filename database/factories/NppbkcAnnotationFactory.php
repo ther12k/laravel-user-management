a4d3e7dcba401c681714d1e1c8ba1aaff8fb3fe3
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-16 15:35:54 +0700

    annotaion factory

---
+++ database/factories/NppbkcAnnotationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\NppbkcAnnotation;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NppbkcAnnotationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = NppbkcAnnotation::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
