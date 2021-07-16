9da240a5fced7d52876f7a66989ea8d124ac229f
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-16 15:39:22 +0700

    changed color

--- resources/views/livewire/form/preview-input.blade.php
+++ resources/views/livewire/form/preview-input.blade.php
@@ -1,4 +1,8 @@
 <div class="grid grid-cols-2">
     <div class="px-4 py-2 font-semibold flex-grow">{{$text}}</div>
+    @if(!isset($nppbk)||!isset($nppbk)||$nppbkc->{$name}== ${$name} )
     <div class="px-4 py-2 flex-grow">{{ ${$name} }}</div>
+    @else
+    <div class="px-4 py-2 flex-grow text-green-700 font-semibold ">{{ ${$name} }}</div>
+    @endif
 </div>            
