8b136cede6dbe0913c85da06cd2d197fc0bd6f6e
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-16 15:39:39 +0700

    status

--- resources/views/livewire/nppbkc/update-status.blade.php
+++ resources/views/livewire/nppbkc/update-status.blade.php
@@ -1,13 +1,9 @@
 <div>
     <h1 class="flex-auto text-xl font-semibold text-purple-700 mb-4">Permohonan Pemeriksaan Lokasi / NPPBKC </h1>
-    <h2 class="flex-auto text-lg font-semibold">No 
-        @if($nppbkc->status_nppbkc<3)
-        {{$nppbkc->no_permohonan_lokasi}}
-        @else
-        {{$nppbkc->no_permohonan}}
-        @endif
-    </h2>
-    @if($nppbkc->status_nppbkc<3)
+    <h2 class="flex-auto text-lg font-semibold">No : {{$nppbkc->no_permohonan}}</h2>
+    @if($nppbkc->status_nppbkc>2)
+    <h3 class="flex-auto text-lg font-semibold">Tgl BA : {{\Carbon\Carbon::parse($nppbkc->tanggal_ba_cek_lokasi)->isoFormat('D MMMM Y')}}</h3>
+    @else
     <h2>Tanggal Kesiapan Cek :  {{$nppbkc->tanggal_kesiapan_cek_lokasi}}</h2>
     @endif
     <div class="flex" >
@@ -16,20 +12,22 @@
         @php
             $class=$nppbkc->status_nppbkc;
             switch ($nppbkc->status_nppbkc){
+                case 0:$class='text-red-500';break;
                 case 1:$class='text-gray-600 hover:text-indigo-500';break;
                 case 2:$class='text-indigo-600 hover:text-yellow-500';break;
                 case 3:$class='text-yellow-600 hover:text-gray-500';break;
-                case 4:$class='text-red-600 hover:text-green-500';break;
+                case 4:$class='text-red-600';break;
                 case 5:$class='text-green-600';
             }
         @endphp
-        @can('viewAllNppbkc')
-            @if($nppbkc->status_nppbkc>3)
+        @can('updateStatusNppbkc')
+            @if($nppbkc->status_nppbkc>3||$nppbkc->status_nppbkc==0)
                 <span class="px-2 rounded {{$class}}">
                     {{nppbkc_status_names($nppbkc->status_nppbkc)}}
                 </span>
             @else
                 <button class="hover:text-indigo-600 flex outline-none" 
+                    {{-- x-on:click="$wire.updateStatus()" --}}
                     onclick='Livewire.emit("openModal", "nppbkc.modal",@json(["id"=>$nppbkc->id]))'>
                     <span class="px-2 rounded {{$class}}">
                         {{nppbkc_status_names($nppbkc->status_nppbkc)}}
@@ -48,4 +46,12 @@
             Diupdate @can('viewAllNppbkc')oleh {{$nppbkc->updatedBy->name}}, @endcan{{\Carbon\Carbon::parse($nppbkc->updated_at)->isoFormat('HH:mm D MMMM Y')}}
     </button>
     @endif
+    
+    @cannot('viewAllNppbkc')
+    @if($nppbkc->status_nppbkc==0)
+    <button class="hover:text-indigo-600 flex outline-none"  x-on:click="$emitUp('editData')">
+        <x-heroicon-o-pencil-alt class="h-6 w-6 {{$class}}" />
+    </button>
+    @endif
+    @endcan
 </div>
