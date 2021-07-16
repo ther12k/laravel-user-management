f5966d9510d97e33bfa773a6e4c8586b9143f7ea
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-16 22:48:56 +0700

    bug route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
                        silahkan klik tombol dibawah untuk melanjutkan
                     </p>
                     <form>
-                    <button class="flex justify-center px-10 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700 transition duration-150 ease-in-out" formaction="{{route('add.nppbkc')}}">
+                    <button class="flex justify-center px-10 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700 transition duration-150 ease-in-out" formaction="{{route('nppbkc.add')}}">
                         Input Permohonan
                     </button>
                     </form>
