557d144d5ce3a4c1bf19b27ae7242422ad819d73
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-16 15:38:41 +0700

    update status modal

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.base')
 
 @section('body')
-<x-header />
-    <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+
+    <div class="flex flex-col justify-center min-h-screen py-5 bg-gray-50 sm:px-6 lg:px-8">
         @yield('content')
 
         @isset($slot)
--- resources/views/livewire/nppbkc-modal.blade.php
+++ resources/views/livewire/nppbkc-modal.blade.php
@@ -1,5 +1,5 @@
 <x-nppbkc-modal>
-    <x-slot name="content">
+    <x-slot name="content" >
         @if($status_nppbkc==1)
         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start">
@@ -27,7 +27,7 @@
             <button wire:click.prevent="setuju_cek()" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
                 Setuju
             </button>
-            <button wire:click.prevent="setuju_cek()" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
+            <button wire:click.prevent="tolak_cek()" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
                 Ditolak
             </button>
             <button wire:click="$emit('closeModal')" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
@@ -36,15 +36,15 @@
         </div>
         @endif
         @if($status_nppbkc==2)
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4" x-data="flatpickr('#datepicker',{dateFormat:'d-m-Y'})">
             <h1 class="text-xl font-semibold text-purple-700 mb-4">Penerimaan Permohonan Penerbitan NPPBKC</h1>
-			<div wire:loading.remove.delay>
+			<div wire:loading.remove.delay >
 
                 @include('livewire.form.textarea',['name'=>'catatan_petugas','text'=>'Catatan Petugas'])
-                @include('livewire.form.input',['name'=>'no_ba','text'=>'Nomor Berita Acara'])
-                @include('livewire.form.input-date',['name'=>'tanggal_ba','text'=>'Tanggal Berita Acara'])
+                @include('livewire.form.input',['name'=>'no_ba_cek_lokasi','text'=>'Nomor Berita Acara'])
+                @include('livewire.form.input-date',['name'=>'tanggal_ba_cek_lokasi','text'=>'Tanggal Berita Acara','id'=>'datepicker'])
                 
-                <div class="md:flex mb-6">
+                <div class="md:flex mb-6" >
                 @foreach ($petugas_files as $name=>$text)
 
                     {{-- @include('livewire.form.upload',['name'=>$name,'text'=>$text]) --}}
@@ -61,7 +61,6 @@
                         </div>
                 @endforeach
                 </div>
-
             </div>
             <div wire:loading.delay class="py-5">
                 @include('livewire.form.skeleton')
@@ -69,7 +68,7 @@
         </div>
         <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <button wire:click.prevent="complete()" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
-                Setuju
+                Proses
             </button>
             <button wire:click="$emit('closeModal')" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                 Cancel
