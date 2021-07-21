60c57b94fb1aed93d0b0957f6e6954769db509b3
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-21 18:23:13 +0700

    soft delete user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Notifications\MailResetPasswordNotification;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
     
     protected $with = ['profile'];
  
