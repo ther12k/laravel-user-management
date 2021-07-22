615a12b99eb6dae06dd03ea4c7844be7141b6553
Rizky Zulkarnaen <rizkyz@gmail.com>
2021-07-22 14:34:08 +0700

    config heroku

--- config/database.php
+++ config/database.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Str;
 
 $DATABASE_URL=parse_url('postgres://ikxfsrekqjyxtb:c7b6ee03223a29677b4330ccc407e8386ba2af03a6dfe6e1b2439bd838df6bd7@ec2-35-174-35-242.compute-1.amazonaws.com:5432/d1lcuaklag5mr0');
+$heroku_mysqlurl = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $heroku_mysqlurl["host"];
+$username = $heroku_mysqlurl["user"];
+$password = $heroku_mysqlurl["pass"];
+$database = substr($heroku_mysqlurl["path"], 1);
 return [
 
     /*
@@ -52,6 +58,24 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'heroku_mysql' => [
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
